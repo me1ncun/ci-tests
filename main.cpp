e3ba8ff3d1766d6061eb5d7bafb50db3f8094fe1
me1ncun <denis09112004@gmail.com>
2024-11-06 13:46:38 +0200

    Calculate first 3 elements

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "func.h"
 int main(){
 	FuncClass obj;
-	std::cout << "FuncA result:" << obj.FuncA() << std::endl;
+	std::cout << "FuncA result:" << obj.FuncA(3) << std::endl;
 	return 0;
 }
  
